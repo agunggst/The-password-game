80067f5aaeafdae28ea38b080238b264579149f0
agunggst <rqz.agastya@gmail.com>
2023-10-10 09:33:21 +0700

    rules done

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="game">
     <div class="content-container">
+      <div class="stopwatch"></div>
       <div class="password-input">
         <div class="label">
           <div class="left"> </div>
@@ -9,22 +10,24 @@
         <input type="text" class="regular-input" v-model="password" placeholder="Type your password here . . .">
       </div>
       <div class="rules">
-        <div class="rule">
-          <div class="head">
-            <CloseIcon class="material-icon" size="14"/> <span>Rule 1</span>
+        <template v-for="(rule, index) in rules" :key="index">
+          <div class="rule" v-if="!rule.status">
+            <div class="head">
+              <CloseIcon class="material-icon" size="14"/> <span>Rule {{ index }}</span>
+            </div>
+            <div class="body">
+              <p>{{ rule.text }}</p>
+            </div>
           </div>
-          <div class="body">
-            <p>Your password must be at least 5 characters.</p>
+          <div class="rule-done" v-else>
+            <div class="head">
+              <DoneIcon class="material-icon" size="14"/> <span>Rule {{ index }}</span>
+            </div>
+            <div class="body">
+              <p>{{ rule.text }}</p>
+            </div>
           </div>
-        </div>
-        <div class="rule-done">
-          <div class="head">
-            <DoneIcon class="material-icon" size="14"/> <span>Rule 2</span>
-          </div>
-          <div class="body">
-            <p>Your password must be at least 5 characters.</p>
-          </div>
-        </div>
+        </template>
       </div>
     </div>
   </div>
@@ -43,11 +46,50 @@ export default {
   data () {
     return {
       password: '',
+      rules: {
+        "1": {
+          text: 'Your password must be at least 5 characters.',
+          status: false
+        },
+        "2": {
+          text: 'Your password must include a number.',
+          status: false
+        },
+        "3": {
+          text: 'Your password must include a special character.',
+          status: false
+        },
+        "4": {
+          text: 'Your password must include at least 1 uppercase letter.',
+          status: false
+        },
+        "5": {
+          text: 'The digits in your password must add up to 20.',
+          status: false
+        }
+      }
     }
   },
   computed: {
     counter () {
       return this.password.length
+    }
+  },
+  watch: {
+    password (value) {
+      // 1
+      if (value.length >= 5) { this.rules['1'].status = true } else { this.rules['1'].status = false }
+      // 2
+      if (/\d/.test(value)) { this.rules['2'].status = true } else { this.rules['2'].status = false }
+      // 3
+      if (/(?=.*?[#?!@$%^&*-])/.test(value)) { this.rules['3'].status = true } else { this.rules['3'].status = false }
+      // 4
+      if (/(?=.*[A-Z])/.test(value)) { this.rules['4'].status = true } else { this.rules['4'].status = false }
+      // 5
+      let numbers = value.replace(/^\D+/g, '').split('').map(item => Number(item))
+      let sum = 0
+      numbers.forEach(number => { if (number) sum += number})
+      if (sum >= 20) { this.rules['5'].status = true } else { this.rules['5'].status = false }
     }
   }
 }
@@ -58,7 +100,7 @@ export default {
   margin: 0 auto;
 }
 .password-input, .rules{
-  padding-top: 150px;
+  padding-top: 120px;
   margin: 0 auto;
   width: 400px;
 }
@@ -117,5 +159,15 @@ export default {
   background-color: var(--regular-green);
   border-bottom-left-radius: 8px;
   border-bottom-right-radius: 8px;
+}
+@media screen and (max-width: 450px){
+  .password-input, .rules{
+    width: calc(100% - 56px);
+    padding-left: 14px;
+    padding-right: 14px;
+  }
+  .password-input{
+    padding-top: 80px;
+  }
 }
 </style>
